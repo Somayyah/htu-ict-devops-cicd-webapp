cfc2409558e2ba0b1926fbd5e85edf8ca3cd5f88
Somayyah <55834191+Somayyah@users.noreply.github.com>
2020-04-21 21:18:03 +0300

    Update setup-environment.sh

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,5 +1,5 @@
 python3 -m venv env
 source env/bin/activate
-pip3 install -r /home/somayyah/htu-ict-devops-cicd-webapp/requirements.txt
+pip3 install -r ~/htu-ict-devops-cicd-webapp/requirements.txt
 export set FLASK_APP=hello_app.webapp
 python3 -m flask run
